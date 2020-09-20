6cf9311dc6b50a7ba9f8fc07b939fe4314596c95
WaluigiBSOD <69278068+WaluigiBSOD@users.noreply.github.com>
2020-09-20 22:04:20 +0200

    Refined a lot of things
    Added more constants, replaced "var" with "const" when declaring them, Player Name is now stored into an array and added a new versioning system.

--- script/dm64decoder.js
+++ script/dm64decoder.js
@@ -18,39 +18,72 @@
 
 // Constants
 
-var PasswordCharacters = "ABCDEFGHJKLMNPQRSTVWXY1234567890";
+const Version = "1.0 Beta";
 
-var TableMaskHigh = [
+const PasswordCharacters = "ABCDEFGHJKLMNPQRSTVWXY1234567890";
+
+const TableMaskHigh = [
     0x0B626835, 0x0763337A, 0x0F4D6F49, 0x06343069
 ];
 
-var TableMaskMiddle = [
+const TableMaskMiddle = [
     0x03DF4B61, 0x040E7254, 0x0A456B4F, 0x092E476F
 ];
 
-var TableMaskLower = [
+const TableMaskLower = [
     0x08C26B8A, 0x0D73B9A1, 0x053AD652, 0x024DF62E
 ];
 
-var TableMaskFrameCountLower = [
+const TableMaskFrameCountLower = [
     0x81021794, 0xF4967A99, 0xB403DEC6, 0x7FD52F56, 0x5086D67F, 0xD10A4924, 0x939AEA49, 0x24925292,
     0x492494A4, 0x92492529, 0x79775FFF, 0x00942FFE, 0xB95EA2BC, 0xC3EB108E, 0xA377F5BF, 0xEFAC4D93,
     0x6307CC7F, 0xBAE8741C, 0x46B6AF5C, 0x8F73890D, 0xFEA85A79, 0x5536EA9C, 0xC7F04155, 0x3A6E4D2D,
     0xC567B808, 0x1AEE23E9, 0x7F8454BA, 0xA7586398, 0x69A0CCE8, 0xE7FF0027, 0xF90A7044, 0x62C46329
 ];
 
-var TableMaskFrameCountUpper = [
+const TableMaskFrameCountUpper = [
     0xE435FBDF, 0xE2384C76, 0xD702AE2A, 0x9E8D9B77, 0xED3B7F7A, 0x3DA8B55B, 0xD8A8F1CA, 0xB42CF9E0,
     0x4FA87E8F, 0xCC992492, 0x53B51492, 0x4924A4B8, 0xFF00CE0F, 0x8ADD0B06, 0x8FA63E2B, 0x78205490,
     0x22350C22, 0x3505266A, 0x33505A8C, 0xD4D5240B, 0x0FEB4FF4, 0x767F5FFE, 0xFAF5B816, 0x27D68FE8,
     0xCCFEB8FF, 0x00A9B536, 0x5B32E2F9, 0xC35FEAC7, 0xD0B7E2D5, 0xD0AE7BEA, 0xCE95D9FD, 0x61F91740
 ];
 
+const GameModeClassic = 0;
+const GameModeScoreAttack = 1;
+const GameModeMarathon = 2;
+
+const GameLevelEasy = 0;
+const GameLevelNormal = 1;
+const GameLevelHard = 2;
+
+const SpeedLow = 0;
+const SpeedMedium = 1;
+const SpeedHigh = 2;
+
+const VirusLevelMinimum = 0;
+const VirusLevelMaximum = 24;
+
+const ScoreMinimum = 0;
+const ScoreMaximum = 999999;
+
+const TimeMinimum = 0;
+const TimeMaximum = 59999;
+
+const NameFontCharacterMinimum = 0;
+const NameFontCharacterMaximum = 322;
+
+const FrameCountMinimum = 0;
+const FrameCountMaximum = 1023;
+
 // Flags
 
 var ValidPassword = false;
 
 // Functions
+
+function _SetVersion() {
+	document.getElementById("version").innerHTML = "Version " + Version;
+}
 
 function _ShowResult() {
 	document.getElementById("error").innerHTML = "";
@@ -111,10 +144,7 @@ function _DecodePassword(Password = "") {
 		var Time = 0;
 		var FrameCount = 0;
 		
-		var NameA = 0;
-		var NameB = 0;
-		var NameC = 0;
-		var NameD = 0;
+		var Name = [0,0,0,0];
 		
 		var TemporaryX;
 		var TemporaryY;
@@ -230,15 +260,15 @@ function _DecodePassword(Password = "") {
 		
 		// Name
 
-		NameB = PasswordChunkMedium & 0xFF;
+		Name[1] = PasswordChunkMedium & 0xFF;
 		PasswordChunkMedium >>= 8;
 
-		NameC = PasswordChunkMedium & 0xFF;
+		Name[2] = PasswordChunkMedium & 0xFF;
 		PasswordChunkMedium >>= 8;
 
-		NameD = PasswordChunkMedium & 0xFF;
+		Name[3] = PasswordChunkMedium & 0xFF;
 
-		NameA = PasswordChunkHigh & 0xFF;
+		Name[0] = PasswordChunkHigh & 0xFF;
 		PasswordChunkHigh >>= 8;
 		
 		// Score (2/2)
@@ -259,17 +289,17 @@ function _DecodePassword(Password = "") {
 		TestCheckSum += ((Score >> 10) & 0x3FF);
 		TestCheckSum += (Time & 0xFF);
 		TestCheckSum += ((Time >> 8) & 0xFF);
-		TestCheckSum += NameA + NameB + NameC + NameD;
+		TestCheckSum += Name[0] + Name[1] + Name[2] + Name[3];
 		TestCheckSum &= 0x3FF;
 		
 		if (CheckSum == TestCheckSum) {
 			// Mode
 			
-			if (GameMode == 0)
+			if (GameMode == GameModeClassic)
 				document.getElementById("mode").innerHTML = "Classic";
-			else if (GameMode == 1)
+			else if (GameMode == GameModeScoreAttack)
 				document.getElementById("mode").innerHTML = "Score Attack";
-			else if (GameMode == 2)
+			else if (GameMode == GameModeMarathon)
 				document.getElementById("mode").innerHTML = "Marathon";
 			else {
 				_WriteError("Invalid game mode");
@@ -279,14 +309,14 @@ function _DecodePassword(Password = "") {
 			
 			// Game Level
 			
-			if (GameMode != 0) {
+			if (GameMode != GameModeClassic) {
 				document.getElementById("leveltext").innerHTML = "Game Level:";
 
-				if (Level == 0)
+				if (Level == GameLevelEasy)
 					document.getElementById("level").innerHTML = "Easy";
-				else if (Level == 1)
+				else if (Level == GameLevelNormal)
 					document.getElementById("level").innerHTML = "Normal";
-				else if (Level == 2)
+				else if (Level == GameLevelHard)
 					document.getElementById("level").innerHTML = "Hard";
 				else {
 					_WriteError("Invalid game level");
@@ -297,7 +327,7 @@ function _DecodePassword(Password = "") {
 				document.getElementById("leveltext").innerHTML = "Virus Level:";
 				document.getElementById("level").innerHTML = Level;
 				
-				if (Level < 0 || Level > 24) {
+				if (Level < VirusLevelMinimum || Level > VirusLevelMaximum) {
 					_WriteError("Invalid virus level");
 					
 					return;
@@ -306,11 +336,11 @@ function _DecodePassword(Password = "") {
 			
 			// Speed
 			
-			if (Speed == 0)
+			if (Speed == SpeedLow)
 				document.getElementById("speed").innerHTML = "Low";
-			else if (Speed == 1)
+			else if (Speed == SpeedMedium)
 				document.getElementById("speed").innerHTML = "Medium";
-			else if (Speed == 2)
+			else if (Speed == SpeedHigh)
 				document.getElementById("speed").innerHTML = "High";
 			else {
 				_WriteError("Invalid speed");
@@ -320,7 +350,7 @@ function _DecodePassword(Password = "") {
 			
 			// Score
 			
-			if (Score < 0 || Score > 999999) {
+			if (Score < ScoreMinimum || Score > ScoreMaximum) {
 				_WriteError("Invalid score");
 				
 				return;
@@ -333,7 +363,7 @@ function _DecodePassword(Password = "") {
 			
 			// Time
 			
-			if (Time < 0 || Time > 59999) {
+			if (Time < TimeMinimum || Time > TimeMaximum) {
 				_WriteError("Invalid time");
 				
 				return;
@@ -361,50 +391,22 @@ function _DecodePassword(Password = "") {
 			
 			document.getElementById("name").innerHTML = "";
 			
-			if (NameA < 0 || NameA > 322) {
-				_WriteError("Invalid name");
+			for (var i=0;i<Name.length;i++) {
+				if (Name[i] < NameFontCharacterMinimum || Name[i] > NameFontCharacterMaximum) {
+					_WriteError("Invalid name");
+					
+					return;
+				}
 				
-				return;
-			}
-			
-			if (NameB < 0 || NameB > 322) {
-				_WriteError("Invalid name");
+				while (Name[i].toString().length < 3)
+					Name[i] = "0" + Name[i];
 				
-				return;
+				document.getElementById("name").innerHTML += "<img class=\"name\" src=font/" + Name[i] + ".png></img>";
 			}
-			
-			if (NameC < 0 || NameC > 322) {
-				_WriteError("Invalid name");
-				
-				return;
-			}
-			
-			if (NameD < 0 || NameD > 322) {
-				_WriteError("Invalid name");
-				
-				return;
-			}
-			
-			while (NameA.toString().length < 3)
-				NameA = "0" + NameA;
-			
-			while (NameB.toString().length < 3)
-				NameB = "0" + NameB;
-			
-			while (NameC.toString().length < 3)
-				NameC = "0" + NameC;
-			
-			while (NameD.toString().length < 3)
-				NameD = "0" + NameD;
-			
-			document.getElementById("name").innerHTML += "<img class=\"name\" src=font/" + NameA + ".png></img>";
-			document.getElementById("name").innerHTML += "<img class=\"name\" src=font/" + NameB + ".png></img>";
-			document.getElementById("name").innerHTML += "<img class=\"name\" src=font/" + NameC + ".png></img>";
-			document.getElementById("name").innerHTML += "<img class=\"name\" src=font/" + NameD + ".png></img>";
 			
 			// Frame Count (modulo 1024)
 			
-			if (FrameCount < 0 || FrameCount > 1023) {
+			if (FrameCount < FrameCountMinimum || FrameCount > FrameCountMaximum) {
 				_WriteError("Invalid frame count");
 				
 				return;
@@ -431,4 +433,5 @@ function _DecodePassword(Password = "") {
 	}
 }
 
+_SetVersion();
 _ResetInput();
